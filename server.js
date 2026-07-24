ad23900cf25d5c25f9169470d369931a0b7e9717
Karl Pepon <karlpeponayala@gmail.com>
2026-07-24 11:23:53 +0800

    Add liquidated KB

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get("/", (req, res) => {
   res.json({
     status: "ok",
     message: "KB API is running",
-    endpoints: ["/orchestrator_kb", "/unilateral_nda_kb", "/non_compete_nda_kb", "/individual_nda_kb"]
+    endpoints: ["/orchestrator_kb", "/unilateral_nda_kb", "/non_compete_nda_kb", "/individual_nda_kb", "/liquidated_nda_kb"]
   });
 });
 
@@ -65,6 +65,11 @@ app.get("/non_compete_nda_kb", (req, res) => {
 // New Individual NDA KB endpoint
 app.get("/individual_nda_kb", (req, res) => {
   return handleKbRequest(req, res, "individual_nda_kb.docx");
+});
+
+// New Liquidated NDA KB endpoint
+app.get("/liquidated_nda_kb", (req, res) => {
+  return handleKbRequest(req, res, "/liquidated_nda_kb.docx");
 });
 
 app.listen(PORT, () => {
